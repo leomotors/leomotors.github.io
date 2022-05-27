aaa5c0711688230e7b5f3639b9202926b8439843
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-27 22:21:19 +0700

    Initial Commit, do not visit yet

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Hello World</h1>
+
+<a href="https://portfolio.leomotors.vercel.app">Portfolio</a>
+<a href="https://resume.leomotors.vercel.app">Resume</a>
+<a href="https://leomotors.gitbook.io/blog">Blog</a>
+<a href="https://repos.leomotors.vercel.app">Repositories List</a>
+
+<p>#สร้างRepoก่อน ทำทีหลัง</p>
