4a4663b4a3211c1f8df54710f09df27eb3e56460
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-07-19 21:41:26 +0700

    remove tests

--- tests/index.spec.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("index page has expected stuff", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.textContent("h1")).toContain("Hello!");
-  expect(await page.textContent("p")).toMatch(/^(?!\<Error)/);
-});
